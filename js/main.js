30a92172c6573b4b4e7f039ec641cb13d26d507a
Zhansultan Chuakbayev <barlibaybro@gmail.com>
2021-01-23 01:36:43 +0600

    v1.1

--- js/main.js
+++ js/main.js
@@ -6,9 +6,13 @@ function getValue() {
 	const price2 = 45000 //evening 18h-22h
 	const price3 = 35000 //night 23h-4h
 	const dtime = document.getElementById("dtime");
-	if (parseInt(adult.value) == 0) {
+	if ((parseInt(adult.value) == 0) && (parseInt(kids.value) == 0)) {
 		result.innerHTML = 0;
-		alert("Children cannot fly without an adult!")
+		alert("Please select the number of passengers!")
+	}
+	else if (parseInt(adult.value) == 0) {
+		result.innerHTML = 0;
+		alert("Children cannot fly without an adult, add at least 1 adult!")
 	}
 	else if ((parseInt(dtime.value) >= 5) && (parseInt(dtime.value) <= 17)) {
 		result.innerHTML = price1 * (parseInt(adult.value) + 0.5 * parseInt(kids.value));
